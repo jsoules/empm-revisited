0a14461093697f35034cf8b1caf175636da0fd65
Jeff Soules <jsoules@flatironinstitute.org>
2026-03-09 15:46:27 -0400

    Add file, no other change

---
+++ test/stacks/test_Volume.py
@@ -0,0 +1,13 @@
+import torch
+from inspect import get_annotations, getmembers
+from types import MethodType
+from unittest.mock import Mock, patch
+from pytest import raises
+
+from empm.parameters import Parameters
+from empm.stacks import Volume
+
+
+
+def test_spharm_normalize():
+    ...
